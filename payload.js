b0d37b7bed0de5f246020dc8046a12af8c4020ee
P0L1T3 <33806905+ManoloZocco@users.noreply.github.com>
2020-09-24 11:12:09 +0200

    Added support for *.panopto.*  addresses

--- payload.js
+++ payload.js
@@ -1,28 +1,27 @@
 chrome.extension.onMessage.addListener(function(msg, _sender, sendResponse) {
 
  	if (msg.action == 'download_video') {
+		
+		if(window.location.href.indexOf("panopto.com") < 1 && window.location.href.indexOf("panopto.eu") < 1) { 
+		alert("Sorry, but I didn't find any videos. Are you sure the address in the search bar is with \"panopto.eu\" or \"panopto.com\" ?"); 
+		exit(); 
+		}
+	
+	// Default parameters to grab video info
+	var name = "";
+	var url = "";
+	var metas = document.getElementsByTagName('meta'); 
+	
+	for (var i=0; i<metas.length; i++) 
+		if (metas[i].getAttribute("name") == "twitter:player:stream") { 
+			console.log(metas[i].getAttribute("content"));
+			url = (metas[i].getAttribute("content").split('?')[0]);
+		} 
 
-		if(window.location.href.indexOf("panopto.eu") < 1) {
-				alert("Sorry, but I didn't find any videos. Are you sure the address in the search bar is with \"youruniversity.cloud.panopto.eu\"?"); 
-				exit();
-			}
-			var metas = document.getElementsByTagName('meta');
-
-			// Default parameters to grab video info
-			var name = "";
-			var url = "";
-
-			for (var i=0; i<metas.length; i++)
-				if (metas[i].getAttribute("name") == "twitter:player:stream") {
-					console.log(metas[i].getAttribute("content"));
-					url = metas[i].getAttribute("content").split('?')[0];
-
-				}
-				else if (metas[i].getAttribute("property") == "og:title") {
-					console.log(metas[i].getAttribute("content"));
-					//window.open(metas[i].getAttribute("content").split('?')[0]);
-					name = metas[i].getAttribute("content").split('?')[0];
-				}
+		else if (metas[i].getAttribute("property") == "og:title") {
+			console.log(metas[i].getAttribute("content"));
+			name = metas[i].getAttribute("content").split('?')[0];
+		}
 
 			// Split the name up (remove invalid characters in file names) && add file format
 			name = name.split('/').join('_'); // Remove slashes
