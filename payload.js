fecee11fe65be650de127d592a11f650f588d831
Not Your Mama <why@ofallthings.xyz>
2020-10-28 05:28:45 -0500

    Fix Spacing

--- payload.js
+++ payload.js
@@ -1,34 +1,34 @@
 chrome.extension.onMessage.addListener(function(msg, _sender, sendResponse) {
 
-	if (msg.action == 'download_video') {
-   // Default parameters to grab video info
-   var name = "";
-   var url = "";
-   var metas = document.getElementsByTagName('meta'); 
-   
-   for (var i=0; i<metas.length; i++) 
-	   if (metas[i].getAttribute("name") == "twitter:player:stream") { 
-		   console.log(metas[i].getAttribute("content"));
-		   url = (metas[i].getAttribute("content").split('?')[0]);
-	   } 
+ 	if (msg.action == 'download_video') {	
+	// Default parameters to grab video info
+	var name = "";
+	var url = "";
+	var metas = document.getElementsByTagName('meta'); 
+	
+	for (var i=0; i<metas.length; i++) 
+		if (metas[i].getAttribute("name") == "twitter:player:stream") { 
+			console.log(metas[i].getAttribute("content"));
+			url = (metas[i].getAttribute("content").split('?')[0]);
+		} 
 
-	   else if (metas[i].getAttribute("property") == "og:title") {
-		   console.log(metas[i].getAttribute("content"));
-		   name = metas[i].getAttribute("content").split('?')[0];
-	   }
+		else if (metas[i].getAttribute("property") == "og:title") {
+			console.log(metas[i].getAttribute("content"));
+			name = metas[i].getAttribute("content").split('?')[0];
+		}
 
-		   // Split the name up (remove invalid characters in file names) && add file format
-		   name = name.split('/').join('_'); // Remove slashes
-		   name = name.split(' ').join('_'); // Remove spaces
-		   name = name.split(':').join('-'); // Remove colons
-		   name = name + ".mp4";
+			// Split the name up (remove invalid characters in file names) && add file format
+			name = name.split('/').join('_'); // Remove slashes
+			name = name.split(' ').join('_'); // Remove spaces
+			name = name.split(':').join('-'); // Remove colons
+			name = name + ".mp4";
 
-		   // Pack into array to send through callback function
-		   var infoArray = [url, name];
+			// Pack into array to send through callback function
+			var infoArray = [url, name];
 
-		   // Callback function
-		   sendResponse(infoArray);
+			// Callback function
+			sendResponse(infoArray);
 
-	 }
+  	}
 
 });
