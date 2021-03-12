c14d1a4c123fc0934bfa8e1ee04884fb48af0221
P0L1T3 <33806905+ManoloZocco@users.noreply.github.com>
2021-03-12 16:35:33 +0100

    Update background.js
    Added support for embed version.

--- background.js
+++ background.js
@@ -2,7 +2,19 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		if(tabs[0].url.indexOf("panopto.com") > 1 || tabs[0].url.indexOf("panopto.eu") > 1) {
-			chrome.tabs.sendMessage(tabs[0].id, {action: "download_video"}, downloadCallback);  
+			if (tabs[0].url.indexOf("Embed.aspx") > 1) {
+                var newurl = tabs[0].url.replace('/Embed.aspx','/Viewer.aspx');
+                chrome.tabs.update(tabs[0].id,{url:newurl});
+        chrome.tabs.onUpdated.addListener(function listener (tabId, info) {
+                if (info.status === 'complete' && tabId === tabs[0].id) {
+                    chrome.tabs.onUpdated.removeListener(listener);
+            chrome.tabs.sendMessage(tabs[0].id, {action: "download_video"}, downloadCallback);
+                }
+            });
+            }
+		else{
+		chrome.tabs.sendMessage(tabs[0].id, {action: "download_video"}, downloadCallback);
+		}  
 		} else if (tabs[0].url.indexOf("panopto.com") == -1 || tabs[0].url.indexOf("panopto.eu") == -1) {
 			alert("Sorry, but I didn't find any videos. Are you sure the address in the search bar is with \"panopto.eu\" or \"panopto.com\" ?");
 		}
